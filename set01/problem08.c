4abfb265de2fb2ad96a4197878c5d42ff925565e
Nitish <nitishs@rvu.edu.in>
2024-01-25 15:47:06 +0530

    test

---
+++ set01/problem08.c
@@ -0,0 +1,51 @@
+/*int input_array_size();
+void input_array(int n, int a[n]);
+int sum_n_array(int n, int a[n]);
+void output(int n, int a[n],int sum);*/
+
+int input_array_size();
+void input_array(int n,int a[n]);
+int sum_n_array(int n,int a[n]);
+void output(int n, int a[n],int sum);
+int main()
+{
+    int n=input_array_size();
+    int array[n];
+    input_array(n,array);
+    int sum=sum_n_array(n,array);
+    output(n,array,sum);
+    return 0;
+}
+int input_array_size()
+{
+    int n;
+printf("ENter the size of the array");
+scanf("%d",&n);
+return n;
+}
+
+void input_array(int n,int a[n])
+{
+    printf ("Enter the elements of the array\n");
+    for(int i=0;i<n;i++){
+    Printf("Element %d:",i+1);
+    scanf("%d",&a[i]);
+    }
+}
+
+int sum_n_array(int n,int a[n])
+{
+    int sum = 0;
+    for(int i = 0;i<n;i++){
+        sum = sum +a[i];
+    }
+    return sum;
+    }
+void output(int n,int a[n],int sum){
+    printf("Array elements are: ");
+    for(int i=0;i<n;i++){
+        printf("%d",a[i]);
+    }
+    Printf("Sum of array elements: \n",sum);
+}
+
