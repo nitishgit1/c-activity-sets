108b8637cfd1c46e34b0d067d7456c6e03bd2603
Nitish <nitishs@rvu.edu.in>
2024-03-12 14:48:42 +0530

    test

--- set02/problem01.c
+++ set02/problem01.c
@@ -1,6 +1,7 @@
 /*void input(float *base, float *height);
 void find_area(float base , float height, float *area);
 void output(float base, float height, float area);*/
+
 #include<stdio.h>
 void input(float *base, float *height);
 void find_area(float base , float height, float *area);
